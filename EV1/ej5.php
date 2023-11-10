fc394f286d24150038b712345ade581dfd77514f
Toni <toniperalb@gmail.com>
2023-11-10 13:35:06 +0100

    DWS

---
+++ EV1/ej5.php
@@ -0,0 +1,4 @@
+<?php
+$num1 = 5;
+$num2 = 6;
+echo $num1 > $num2 ? "El numero 1 es mayor al numero 2" : "El numero 2 es mayor al numero 1";
---
+++ EV1/ej6.php
@@ -0,0 +1,4 @@
+<?php
+$num1 = 5;
+$num2 = 5;
+echo ($num1 > $num2) ? "El numero 1 es mayor al numero 2" : (($num1 < $num2) ? "El numero 2 es mayor al numero 1" : "Los dos numeros son iguales");
